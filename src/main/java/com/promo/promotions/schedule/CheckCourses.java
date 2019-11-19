7ed7342a403a3cc2f56d7189d1a9720214ea642c
Adam_Z <youngtoss38@gmail.com>
2019-11-19 15:21:06 +0100

    schedule added

---
+++ src/main/java/com/promo/promotions/schedule/CheckCourses.java
@@ -0,0 +1,18 @@
+package com.promo.promotions.schedule;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class CheckCourses {
+
+
+
+    @Scheduled(fixedRate = 5000)
+    public void checkDollarCourse(){
+
+        
+
+    }
+
+
+}
