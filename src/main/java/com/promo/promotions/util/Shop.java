866883a6447e8a2ff5e65c17644c67fd76ce2ba4
Adam_Z <youngtoss38@gmail.com>
2019-12-03 14:00:26 +0100

    shoplist and shop created

---
+++ src/main/java/com/promo/promotions/util/Shop.java
@@ -0,0 +1,96 @@
+package com.promo.promotions.util;
+
+import com.promo.promotions.enums.CategoriesTypes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Shop {
+
+    private String shopName;
+    private String originUrl;
+    private CategoriesTypes[] categories;
+    private List<CategoriesTypes> categoriesTypesList;
+    private String pathPrice;
+    private String pathName;
+    private String getByXPathParent;
+    private boolean needsOriginUrlTohref;
+
+
+    public Shop(String shopName,String originUrl, CategoriesTypes[] categories, String pathPrice, String pathName,
+                String getByXPathParent, boolean needsOriginUrlTohref) {
+        this.shopName = shopName;
+        this.originUrl = originUrl;
+        this.categories = categories;
+        this.categoriesTypesList = new ArrayList<>(Arrays.asList(categories));
+        this.pathPrice = pathPrice;
+        this.pathName = pathName;
+        this.getByXPathParent = getByXPathParent;
+        this.needsOriginUrlTohref = needsOriginUrlTohref;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getOriginUrl() {
+        return originUrl;
+    }
+
+    public void setOriginUrl(String originUrl) {
+        this.originUrl = originUrl;
+    }
+
+    public CategoriesTypes[] getCategories() {
+        return categories;
+    }
+
+    public void setCategories(CategoriesTypes[] categories) {
+        this.categories = categories;
+    }
+
+    public List<CategoriesTypes> getCategoriesTypesList() {
+        return categoriesTypesList;
+    }
+
+    public void setCategoriesTypesList(List<CategoriesTypes> categoriesTypesList) {
+        this.categoriesTypesList = categoriesTypesList;
+    }
+
+    public String getPathPrice() {
+        return pathPrice;
+    }
+
+    public void setPathPrice(String pathPrice) {
+        this.pathPrice = pathPrice;
+    }
+
+    public String getPathName() {
+        return pathName;
+    }
+
+    public void setPathName(String pathName) {
+        this.pathName = pathName;
+    }
+
+    public String getGetByXPathParent() {
+        return getByXPathParent;
+    }
+
+    public void setGetByXPathParent(String getByXPathParent) {
+        this.getByXPathParent = getByXPathParent;
+    }
+
+    public boolean isNeedsOriginUrlTohref() {
+        return needsOriginUrlTohref;
+    }
+
+    public void setNeedsOriginUrlTohref(boolean needsOriginUrlTohref) {
+        this.needsOriginUrlTohref = needsOriginUrlTohref;
+    }
+}
---
+++ src/main/java/com/promo/promotions/util/ShopList.java
@@ -0,0 +1,33 @@
+package com.promo.promotions.util;
+
+import com.promo.promotions.enums.CategoriesTypes;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.promo.promotions.enums.CategoriesTypes.replacementvalue;
+
+@Component
+public class ShopList {
+
+    private List<Shop> shopList = new ArrayList<>();
+
+    public ShopList() {
+
+        shopList.add(new Shop("Allegro","https://allegro.pl", new CategoriesTypes[]{new CategoriesTypes(CategoriesTypes.Categories.All, "https://allegro.pl/listing?string=", false),
+                new CategoriesTypes(CategoriesTypes.Categories.Electronic, "https://allegro.pl/kategoria/elektronika?string=", false),
+                new CategoriesTypes(CategoriesTypes.Categories.Cloths, "https://allegro.pl/kategoria/moda?string=", false),
+                new CategoriesTypes(CategoriesTypes.Categories.Books, "https://allegro.pl/kategoria/ksiazki-i-komiksy?string=", false),
+                new CategoriesTypes(CategoriesTypes.Categories.Cars, "https://allegro.pl/kategoria/motoryzacja?string=", false)},".//span[@class='fee8042']",
+                ".//h2[@class='ebc9be2 _5087f6f']/a","//div[@class='b659611 _307719f']",false));
+
+
+        shopList.add(new Shop("Amazon","https://amazon.com",new CategoriesTypes[]{new CategoriesTypes(CategoriesTypes.Categories.All, "https://www.amazon.com/s?k=", false),
+                new CategoriesTypes(CategoriesTypes.Categories.Electronic, "https://www.amazon.com/s?k=" + replacementvalue + "&i=electronics", true),
+                new CategoriesTypes(CategoriesTypes.Categories.Books, "https://www.amazon.com/s?k=" + replacementvalue + "&i=stripbooks", true),
+        },".//span[@class='a-price']/span", ".//a[@class='a-link-normal a-text-normal']","//div[@class='sg-col-inner']",true));
+
+        
+    }
+}
